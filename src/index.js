ccef7bb9fa59dba49665ba151a59578b8771f273
mptrv01 <mihailpettrow@gmail.com>
2022-12-05 12:00:09 +0200

    update: some update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import "./scss/app.scss";
 import Application from "./js/Application";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-  const app = new Application();
+
+   const app = new Application();
   // Used to access the app instance by the automated tests
-  app.setEmojis(["🐒", "🦍", "🦧"]);
-  app.addBananas();
+  app.setEmojis(["🐒", "🦍", "🦧"],
+   a = document.getElementById("emojis"), 
+           array.forEach((m) => a.value = m ),
+  );
+  app.addBananas(array.map(array.forEach(element => {
+    element.set(banana);        
+ }))) 
   window.__JS_APP = app;
 });
